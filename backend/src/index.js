718b251501d02f032460515a1f8baec4ec99ddb9
brunosigomes <brunosigomes@gmail.com>
2020-03-25 06:30:44 -0300

    Commenting Code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
+// Loading Express, Cors and Routes
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
+// Creating Application
 const app = express();
-const port = 3333;
 
+// Settting Application
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port);
+// Setting Port for Applicatio "localhost:port"
+app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
+// Importing Express
 const express = require('express');
 
+// Loading Controllers
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+// Loading Router
 const routes = express.Router();
 
+// Creating Routes
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
@@ -18,4 +22,5 @@ routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
 
+// Exporting Routes
 module.exports = routes;
